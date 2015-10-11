00431ae0ce674b47fb24aa51eba56033fc02f75a
Nick Riley <nick@sparkida.com>
2015-10-11 04:57:28 -0700

    app entry updated for config 1:3

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 /**
- * Nuora v0.4.1
+ * Nuora v1.2.0
  * Author: Nicholas Riley
  */
 ;(function () {
     "use strict";
     var path = require('path'),
-        config,
-        Nuora;
-    try {
-        config = require('./config/nuora.local');
-    } catch (errConfigLoad) {
-        config = require('./config/nuora');
-    }
+		config = require('./config/nuora'),
+		Nuora = require('./lib/nuora');
     config.opts = require('./lib/opts');
     config.env = (config.opts.production || process.env.NODE_ENV === 'production') ? 'production' : 'dev';
     config.apppath = __dirname;
     config.startpath = path.resolve(process.cwd());
-    Nuora = require('./lib/nuora');
     module.exports = Nuora.initialize(config, require.main !== module);
 }());
 
